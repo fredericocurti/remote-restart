d0739a95fb468ebc3eadf943b2808e0ce7a07257
fredericocurti <frederico.vilelacurti@gmail.com>
2020-09-09 09:23:13 -0300

    first init

---
+++ Front/public/main.js
@@ -0,0 +1,55 @@
+let clientsDiv
+
+const onClickBtn = (client) => {
+    const date = new Date()
+    firebase.database().ref(`/clients/${client}/lastRestart`).set(date.toJSON())
+    update()
+}
+
+const update = () => {
+    firebase.database().ref('/clients').once('value', snapshot => {
+        clientsDiv.innerHTML = ""
+        Object.entries(snapshot.val()).map(([client, val]) => {
+            const { lastPing, lastRestart } = val
+
+            const deltaMinutes = (new Date() - new Date(lastPing)) / 60000
+
+            let status = "ONLINE"
+            if (deltaMinutes > 1) {
+                status = "OFFLINE"
+            }
+
+            clientsDiv.innerHTML += `
+            <span class="card">
+                <div><b>${status === "ONLINE" ? "🟢" : "🔴"} ${client} (${status})</b></div>
+                <div class="divider"></div>
+                Last Ping ${new Date(lastPing)}<br>
+                Last Restart ${lastRestart === "never" ? "never" : new Date(lastRestart)}<br>
+                <button onclick="onClickBtn('${client}')"> Restart </button>
+            </span>
+            `
+            if (status === "OFFLINE") {
+                clientsDiv.firstElementChild.lastElementChild.remove()
+            }
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    clientsDiv = document.querySelector("#clients")
+    // // The Firebase SDK is initialized and available here!
+    update()
+    setInterval(update, 5000)
+    document.querySelectorAll("button").forEach(b => b.addEventListener("click", (ev) => { }))
+
+    try {
+        let app = firebase.app();
+        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+        // document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+        document.getElementById('load').innerHTML = ""
+
+    } catch (e) {
+        console.error(e);
+        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+    }
+});
---
+++ RemoteRestart/Program.cs
@@ -0,0 +1,119 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteRestart
+{
+    public class ProcessIcon : IDisposable
+    {
+        public NotifyIcon Icon { get; set; }
+
+        public ProcessIcon()
+        {
+            Icon = new NotifyIcon();
+        }
+
+        public void Display()
+        {
+            var menu = new ContextMenu();
+            var mnuExit = new MenuItem("Exit");
+            menu.MenuItems.Add(0, mnuExit);
+            mnuExit.Click += new EventHandler(mnuExit_Click);
+
+            Icon.ContextMenu = menu;
+            Icon.Text = "Remote Restart\nStatus: running";
+            Icon.Visible = true;
+            Icon.Icon = SystemIcons.Shield;
+        }
+
+        public void Dispose()
+        {
+            Icon.Dispose();
+        }
+
+        static void mnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+
+    class Program
+    {
+        public static IFirebaseClient Client { get; set; }
+        public static string Path { get; set; } = $"clients/{Environment.MachineName}";
+
+        public static ProcessIcon Pi { get; set; } = null;
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0] == "INSTALLER") { Process.Start(Application.ExecutablePath); return; }
+
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                BasePath = "https://remote-restart.firebaseio.com/"
+            };
+
+            Client = new FirebaseClient(config);
+
+
+            Thread notifyThread = new Thread(
+            delegate ()
+            {
+                Pi = new ProcessIcon();
+                Pi.Display();
+                Application.Run();
+            }
+            );
+
+
+            notifyThread.Start();
+
+            Init();
+            Run();
+            Ping().Wait();
+        }
+
+
+        private static void Init()
+        {
+            var exists = Client.Get($"{Path}/lastRestart").Body == "null";
+            if (exists)
+            {
+                Client.Set($"{Path}/lastRestart", "never");
+            }
+        }
+
+        private static async Task Ping()
+        {
+            while (true)
+            {
+                await Client.SetAsync($"{Path}/lastPing", DateTime.Now);
+                Thread.Sleep(30000);
+            }
+        }
+
+
+        private static async Task Run()
+        {
+
+            bool ready = false;
+            EventStreamResponse response = await Client.OnAsync($"{Path}/lastRestart", null, (sender, args, context) => {
+                if (ready)
+                {
+                    Process.Start("shutdown.exe", "-r -f -t 5");
+                }
+                ready = true;
+            });
+        }
+    }
+}
